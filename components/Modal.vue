63f717f484fa747598ab03c10aefb50e4766f589
Denis Matern <denismatern@gmail.com>
2024-08-05 20:24:22 +0300

    feature: mdi icons, modal

---
+++ components/Modal.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiClose } from "@mdi/js";
+
+const isOpen = defineModel(false);
+</script>
+
+<template>
+    <div
+        v-if="isOpen"
+        class="min-w-full min-h-screen flex items-center justify-center fixed blur-sm"
+    >
+        <div class="w-full max-w-96 p-4 relative bg-white">
+            <button
+                class="absolute top-4 right-4 hover:text-blue-600"
+                type="button"
+                @click="isOpen = false"
+            >
+                <SvgIcon type="mdi" :path="mdiClose" />
+            </button>
+
+            <slot />
+        </div>
+    </div>
+</template>
