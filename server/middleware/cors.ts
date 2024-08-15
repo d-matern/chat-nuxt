e50f45656e9772134ee19572da751d7d5aa311cb
Denis Matern <denismatern@gmail.com>
2024-08-15 23:58:24 +0300

    fix: cors

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,20 +1,35 @@
 export default defineEventHandler((event) => {
-    const allowedOrigins = [`exp://${process.env.SECRET_KEY_MOBILE_APP}`];
+    // const allowedOrigins = [`exp://${process.env.SECRET_KEY_MOBILE_APP}`];
 
-     if (event.node.req.headers.origin && allowedOrigins.includes(event.node.req.headers.origin)) {
-        const headers = {
-            "Access-Control-Allow-Origin": event.node.req.headers.origin,
-            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        };
+    //  if (event.node.req.headers.origin && allowedOrigins.includes(event.node.req.headers.origin)) {
+    //     const headers = {
+    //         "Access-Control-Allow-Origin": event.node.req.headers.origin,
+    //         "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+    //         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    //     };
 
-        for (const [key, value] of Object.entries(headers)) {
-            event.node.res.setHeader(key, value);
-        }
+    //     for (const [key, value] of Object.entries(headers)) {
+    //         event.node.res.setHeader(key, value);
+    //     }
 
-        if (event.node.req.method === "OPTIONS") {
-            event.node.res.statusCode = 204;
-            event.node.res.end();
-        }
+    //     if (event.node.req.method === "OPTIONS") {
+    //         event.node.res.statusCode = 204;
+    //         event.node.res.end();
+    //     }
+    // }
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    };
+
+    for (const [key, value] of Object.entries(headers)) {
+        event.node.res.setHeader(key, value);
+    }
+
+    if (event.node.req.method === "OPTIONS") {
+        event.node.res.statusCode = 204;
+        event.node.res.end();
     }
 });
