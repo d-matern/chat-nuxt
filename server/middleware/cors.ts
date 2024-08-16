531f07cefa891fa45c3de5737ac492d6fb04fea2
Denis Matern <denismatern@gmail.com>
2024-08-16 13:28:06 +0300

    fix: comment

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,23 +1,4 @@
 export default defineEventHandler((event) => {
-    // const allowedOrigins = [`exp://${process.env.SECRET_KEY_MOBILE_APP}`];
-
-    //  if (event.node.req.headers.origin && allowedOrigins.includes(event.node.req.headers.origin)) {
-    //     const headers = {
-    //         "Access-Control-Allow-Origin": event.node.req.headers.origin,
-    //         "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-    //         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    //     };
-
-    //     for (const [key, value] of Object.entries(headers)) {
-    //         event.node.res.setHeader(key, value);
-    //     }
-
-    //     if (event.node.req.method === "OPTIONS") {
-    //         event.node.res.statusCode = 204;
-    //         event.node.res.end();
-    //     }
-    // }
-
     const headers = {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
